f24a432304b1ccb58e29e2de6fd0631326f87b44
Jhonathan <guarino90@gmail.com>
2023-05-08 21:15:58 -0300

    Recreate page

--- js/nav-bar.js
+++ js/nav-bar.js
@@ -1,5 +1,6 @@
 const toggleBtn = document.querySelector(".nav-toggle");
 const actionWrapper = document.querySelector(".action-wrapper");
+const body = document.querySelector("body");
 
 toggleBtn.addEventListener("click", () => {
   actionWrapper.classList.toggle("show-nav");
